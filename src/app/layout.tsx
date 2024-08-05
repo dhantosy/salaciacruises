35ee68438572ffb0b52ac6f28e4313d68b99d716
dhanto santika <dhantosy@gmail.com>
2024-08-05 14:27:40 +0800

    feat: updated meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Playfair_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Salacia Cruises | Divine Experience",
+  title: "Salacia Cruises | A Divine Experience",
   description: "Embark on an unforgettable underwater adventure with our luxury liveaboard cruises. Discover pristine dive sites, encounter incredible marine life, and relax in style. Our fleet of world-class vessels offers unparalleled comfort and personalized service. Book your dream dive vacation today!",
   keywords: "luxury liveaboard, liveaboard cruises, dive cruises, scuba diving liveaboard, diving vacations, underwater adventures, liveaboard holidays, dive liveaboard, luxury dive trips, liveaboard travel, diving cruises, liveaboard experience, ocean cruises, dive boat vacations, luxury yacht cruises",
 };
